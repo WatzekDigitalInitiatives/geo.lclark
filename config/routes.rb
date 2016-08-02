1d0cfae2bcc4a66ea3e45b741810c79dce7256e1
Rishi Javia <rishi.javia.3895@gmail.com>
2016-08-02 16:50:10 -0700

    rerouting /geoserver to geoserver

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: "catalog#index"
   blacklight_for :catalog
   devise_for :users
+
+  get "/geoserver" => redirect("http://52.42.227.39/web/")
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
